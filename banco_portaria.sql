6682ea67aab458b85e9b059606e7f8929f9b84cc
Lucas Rafael Menezes <menezeslucasrafael@gmail.com>
2026-07-18 17:34:29 -0300

    Delete banco_portaria.sql

--- banco_portaria.sql
+++
@@ -1,8 +0,0 @@
--- 1. Criar o banco de dados dedicado para o seu projeto
-CREATE DATABASE db_portaria;
-GO
-
--- 2. Entrar no banco de dados criado (para garantir que as tabelas vão para o lugar certo)
-USE db_portaria;
-GO
-
